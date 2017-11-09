7fe81f5fb219db5c6d36eab6014f39c31355c1d4
Nicola Girardi <idrarig.alocin@gmail.com>
2017-11-09 21:18:32 +0000

    cloud/storage/openstack: Storage implementation
    Updates upspin/upspin#485
    
    Change-Id: I26766770b9006637aa20712c2c1aaf69cabbb5ef

---
+++ cmd/upspinserver-openstack/main.go
@@ -0,0 +1,18 @@
+// Command upspinserver-openstack is a combined DirServer and
+// StoreServer for use on stand-alone machines. It provides the
+// production implementations of the dir and store servers
+// (dir/server and store/server) with support for storage in
+// OpenStack e.g. OVH Object Storage.
+package main // import "openstack.upspin.io/cmd/upspinserver-openstack"
+
+import (
+	_ "openstack.upspin.io/cloud/storage/openstack"
+
+	"upspin.io/cloud/https"
+	"upspin.io/serverutil/upspinserver"
+)
+
+func main() {
+	ready := upspinserver.Main()
+	https.ListenAndServe(ready, https.OptionsFromFlags())
+}
